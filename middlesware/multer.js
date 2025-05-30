b93aeb7d4e20d6e329a471d1f029dc7f766cfea6
Dhanshri Pawade <pd8806562875@gmail.com>
2025-05-30 16:47:24 +0530

    multer install

---
+++ middlesware/multer.js
@@ -0,0 +1,12 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+const maxSize = 1024 * 1024 * 100;
+const upload = multer({storage , limits : {fileSize : maxSize }});
+
+const fieldsupload = upload.fields([
+     {name : "document" }
+    
+])
+
+export default fieldsupload;
